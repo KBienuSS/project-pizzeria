904f08fcb63fdc4e28639944d588a9a1e1e3f0d9
Kamil Bieniek <bienius.234@gmail.com>
2026-01-14 18:25:13 +0100

    Change local data to data from server API

--- dist/js/script.js
+++ dist/js/script.js
@@ -79,6 +79,11 @@ const settings = {
     totalNumber: 0,
     subtotalPrice: 0,
   },
+  db: {
+    url: '//localhost:3131',
+    products: 'products',
+    orders: 'orders',
+  },
   // CODE ADDED END
 };
 
@@ -532,7 +537,7 @@ const templates = {
       const thisApp = this;
       console.log('thisApp.data', thisApp.data);
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
@@ -545,14 +550,28 @@ const templates = {
 
     initData: function() {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+      
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+          /*saved parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /*execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data ', JSON.stringify(thisApp.data));
     },
 
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
